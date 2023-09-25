32ce88e718b678ed2cc0cd649535ac9fc5669546
Syed Raihanuzzaman <syed15-10568@diu.edu.bd>
2023-09-25 13:25:09 +0600

    Navigation Drawer Added Successfully

--- app/src/main/java/com/raihan/bottomnavapsfragment/MainActivity.kt
+++ app/src/main/java/com/raihan/bottomnavapsfragment/MainActivity.kt
@@ -2,25 +2,59 @@ package com.raihan.bottomnavapsfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.raihan.bottomnavapsfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var drawerLayout:DrawerLayout
+    private lateinit var actionBarDrawerToggle:ActionBarDrawerToggle
+    val homeFragment = Home()
+    val personFragment = Person()
+    val settingsFragment = Settings()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-      // Assign fragment to Variable
-        val homeFragment = Home()
-        val personFragment = Person()
-        val settingsFragment = Settings()
+        // Assign drawler layout to actionbardrawertoggle
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.mydrawerlayout,R.string.nav_open,R.string.nav_close)
+
+        binding.mydrawerlayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
 
       // Set a fragment to Default that will show in front
         setCurrentFragment(homeFragment)
 
-     // Set Navigation item selected Listener for get the navigation translation
+        binding.navview.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->{
+                    setCurrentFragment(homeFragment)
+                    //close drawer when we click new item
+                    binding.mydrawerlayout.closeDrawers()
+                }
+
+                R.id.person ->{
+                    setCurrentFragment(personFragment)
+                    binding.mydrawerlayout.closeDrawers()
+                }
+                R.id.settings ->{
+                    setCurrentFragment(settingsFragment)
+                    binding.mydrawerlayout.closeDrawers()
+                }
+            }
+            true
+        }
+
+
+        // Set Navigation item selected Listener for get the navigation translation
         binding.bottomnav.setOnNavigationItemSelectedListener{
             // Used 'When' to get id by Condition
             when(it.itemId){
@@ -32,13 +66,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-   // Create a Method for set fragment and simplyfy the code for reassign fragment
-      private fun setCurrentFragment(fragment: Fragment) {
-
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragmentlayout,fragment)
-            commit()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            true
         }
-
+        else super.onOptionsItemSelected(item)
     }
+
+
+   // Create a Method for set fragment and simplyfy the code for reassign fragment
+   //create a method for set fragment and simplify the code for reassign
+   private fun setCurrentFragment(fragment: Fragment){
+       supportFragmentManager.beginTransaction().apply {
+           replace(R.id.fragmentlayout, fragment)
+           commit()
+       }
+   }
+
 }
